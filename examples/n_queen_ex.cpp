5f7aa9c5c94f80da793f5d57294ee2fd1c281a12
xiaobaigou1000 <xiaobaigou2000@qq.com>
2020-12-04 00:30:22 +0800

    n queen example for 0 ~ 30 board size

--- examples/n_queen_ex.cpp
+++ examples/n_queen_ex.cpp
@@ -1,17 +1,29 @@
 #include "n_queen.h"
+#include "complexity_tool.h"
 #include <iostream>
 
 int main()
 {
-    NQueen n_queen{30};
-    bool result = n_queen.calculate();
-    auto board = n_queen.getBoardCopy();
-    for (auto row : board)
+    for (int64_t i = 8; i < 30; i++)
     {
-        for (auto col : row)
+        std::cout << "current board size: " << i << std::endl;
+        ComplexityMeasurement cm;
+        cm.run();
+
+        NQueen n_queen{i};
+        bool result = n_queen.calculate();
+        auto board = n_queen.getBoardCopy();
+        for (auto row : board)
         {
-            std::cout << col << " ";
+            for (auto col : row)
+            {
+                std::cout << col << " ";
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
+
+        cm.stop();
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(cm.getPassedTime()).count() << std::endl;
+        std::cout << cm.getMemoryUsage() << std::endl;
     }
 }
