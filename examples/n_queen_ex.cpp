d8e57dba47465afac19ec2fde21705cde6d3f038
xiaobaigou1000 <xiaobaigou2000@qq.com>
2020-12-03 18:14:12 +0800

    n queen(finished)

---
+++ examples/n_queen_ex.cpp
@@ -0,0 +1,17 @@
+#include "n_queen.h"
+#include <iostream>
+
+int main()
+{
+    NQueen n_queen;
+    bool result = n_queen.calculate();
+    auto board = n_queen.getBoardCopy();
+    for (auto row : board)
+    {
+        for (auto col : row)
+        {
+            std::cout << col << " ";
+        }
+        std::cout << std::endl;
+    }
+}
--- n_queen/include/n_queen.h
+++ n_queen/include/n_queen.h
@@ -6,19 +6,74 @@ class NQueen
 public:
     NQueen() : board(boardSize, std::vector<int32_t>(boardSize, 0)) {}
 
+    bool calculate()
+    {
+        return findQueen(0);
+    }
+
+    bool findQueen(int64_t row)
+    {
+        if (row >= boardSize)
+        {
+            return true;
+        }
+
+        for (int64_t col = 0; col < boardSize; col++)
+        {
+            if (checkValid(row, col))
+            {
+                board[row][col] = 1;
+                bool result = findQueen(row + 1);
+                if (result == false)
+                {
+                    board[row][col] = 0;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    std::vector<std::vector<int32_t>> getBoardCopy()
+    {
+        return board;
+    }
+
 private:
     std::vector<std::vector<int32_t>> board;
-    static constexpr size_t boardSize = 8;
-    bool checkValid(size_t row, size_t col)
+    static constexpr int64_t boardSize = 8;
+    bool checkValid(int64_t row, int64_t col)
     {
         bool val = true;
-        if (board[row][col] != 0)
+        for (int64_t currentRow = row - 1, leftCol = col - 1, rightCol = col + 1; currentRow >= 0; --currentRow, --leftCol, ++rightCol)
         {
-            val = false;
-        }
-        for (size_t i = 0; i < col; i++)
-        {
+            if (leftCol >= 0)
+            {
+                if (board[currentRow][leftCol] != 0)
+                {
+                    val = false;
+                    break;
+                }
+            }
 
+            if (rightCol < boardSize)
+            {
+                if (board[currentRow][rightCol] != 0)
+                {
+                    val = false;
+                    break;
+                }
+            }
+
+            if (board[currentRow][col] != 0)
+            {
+                val = false;
+                break;
+            }
         }
+        return val;
     }
-}
+};
